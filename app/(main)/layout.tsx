eb0bb988905607620c129d3d8dc2d8df71205799
Md Ariful Islam <arif171042@gmail.com>
2025-11-20 04:47:35 +0600

    update

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,12 +1,17 @@
 import { getSession } from "@/lib/getSession";
 import Navbar from "./_components/navbar";
+import { redirect } from "next/navigation";
 
 const MainLayout = async ({ children }: LayoutProps<"/">) => {
   const user = await getSession()
 
+  if (!user) {
+    return redirect("/auth/login");
+  }
+
   return (
     <div className="flex min-h-screen flex-col bg-zinc-50 dark:bg-background">
-      <Navbar user={user || null}/>
+      <Navbar user={user || null} />
       {children}
     </div>
   );
